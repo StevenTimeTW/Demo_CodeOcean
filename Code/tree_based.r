7752bd891157b5d4e55e538f055afe51cb929ee5
Steventime <32660539+Steventime0430@users.noreply.github.com>
2021-05-27 18:25:04 +0800

    data, decision_tree

---
+++ Code/tree_based.r
@@ -0,0 +1,52 @@
+
+# ---Library---
+
+library(dplyr) 
+library(rpart) # Decision Tree
+library(rpart.plot) # Decision Tree
+
+# ---Import Data---
+data.df <- read.csv(file="../data/train.csv", header = T) 
+
+# ---Data Pre-processing---
+data.df <- data.df %>% select("Survived", "Pclass", "Sex",
+                                "Age","Fare") %>%
+                       mutate(Survived = factor(Survived, levels = c(0,1), labels = c('No','Yes'))) %>%
+                       mutate(Pclass = factor(Pclass, levels = c(1,2,3),labels = c('Upper','Middle','Lower'))) %>%
+                       mutate(Sex = factor(Sex, levels = c('male','female'),labels = c('Male','Female')))
+
+
+train.index <- sample(x=1:nrow(data.df), size=ceiling(0.8*nrow(data.df) ))
+train.df <- data.df[train.index, ]
+test.df <- data.df[-train.index, ]
+
+
+# ---Build Decision Tree Model---
+
+## Build Decision Tree Model
+cart.model<- rpart(Survived ~. , 
+                   data=train.df)
+cart.model
+
+## Plot the Decision Tree
+prp(cart.model,         # 模型
+    faclen=0,           # 呈現的變數不要縮寫
+    fallen.leaves=TRUE, # 讓樹枝以垂直方式呈現
+    shadow.col="gray", extra =4,main="Decision Tree")
+
+## Predict on Test Dataset
+pred <- predict(cart.model, newdata=test.df, type ="class")
+confusion_matrix <- table(real=test.df$Survived, predict=pred)
+confusion_matrix
+accuracy <- (confusion_matrix[1,1]+confusion_matrix[2,2])/nrow(test.df)
+accuracy # 0.787
+
+
+
+
+
+
+
+
+
+
